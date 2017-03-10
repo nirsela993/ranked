84006f797d850fddc82ac712698b524c18cd0a01
Nir Sela <nirsela@Nirs-MacBook-Pro.local>
2017-03-09 16:20:02 -0800

    add map view

---
+++ ranked/MapViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MapViewController.swift
+//  ranked
+//
+//  Created by Nir Sela on 3/9/17.
+//  Copyright © 2017 Nir Sela. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var Map: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
