d5a7d037c03358743f275c91dbc57d49d36ef777
jezuzcrist <vfxlord@gmail.com>
2017-03-11 04:52:04 -0800

    basic categories page

---
+++ ranked/CategoriesPage/CategoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCell.swift
+//  ranked
+//
+//  Created by macDevMachinVB on 11/03/2017.
+//  Copyright © 2017 Nir Sela. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+}
