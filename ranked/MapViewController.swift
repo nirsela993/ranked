d506f00eb2939b34282bffb15d350cf8d50e9ef8
Victor Zag <victorzag@Victors-iMac.local>
2017-03-11 01:46:03 -0800

    map works like magick cuz of reasons

--- ranked/MapViewController.swift
+++ ranked/MapViewController.swift
@@ -10,15 +10,30 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate{
 
     @IBOutlet weak var Map: MKMapView!
+    
+    let locationManager = CLLocationManager();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
         
+        self.locationManager.delegate = self;
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization();
+        self.locationManager.startUpdatingLocation();
         
+        let location = CLLocationCoordinate2DMake(32, 30);
+        let annotation = MKPointAnnotation();
+        annotation.coordinate = location;
+        annotation.title = "HOME";
+        annotation.subtitle = "Sweet Home";
+        
+        self.Map.addAnnotation(annotation);
+        
+        self.Map.showsUserLocation = true;
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,7 +41,21 @@ class MapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    // MARK: - Location Delagate Methods
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0];
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude);
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1));
+        self.Map.setRegion(region, animated: true);
+        self.locationManager.stopUpdatingLocation();
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Errors: " + error.localizedDescription);
+    }
+    
+    
     /*
     // MARK: - Navigation
 
