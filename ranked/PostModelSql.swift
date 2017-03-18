3b359f3a8839af0dfdbbed78091b1103e44e2bf8
Unknown <Victorzag@gmail.com>
2017-03-18 07:35:16 -0700

    get posts by category sql added

--- ranked/PostModelSql.swift
+++ ranked/PostModelSql.swift
@@ -120,4 +120,36 @@ extension Post{
         return posts
     }
     
+    static func getAllPostsByCategoryFromLocalDb(categoryName:String ,database:OpaquePointer?)->[Post]{
+        var posts : [Post] = []
+        var sqlite3_stmt: OpaquePointer? = nil
+        if (sqlite3_prepare_v2(database,"SELECT * from " + TABLE + " WHERE " + CATEGORY + " = ?;",-1,&sqlite3_stmt,nil) == SQLITE_OK){
+            while(sqlite3_step(sqlite3_stmt) == SQLITE_ROW){
+                
+                let category = categoryName.cString(using: .utf8)
+                sqlite3_bind_text(sqlite3_stmt, 1, category,-1,nil);
+                
+                let postId = String(validatingUTF8:sqlite3_column_text(sqlite3_stmt,0))
+                let authorNickname = String(validatingUTF8:sqlite3_column_text(sqlite3_stmt,1))
+                let picture = String(validatingUTF8:sqlite3_column_text(sqlite3_stmt,2))
+                let likes = Int(sqlite3_column_int(sqlite3_stmt,4))
+                let dislikes = Int(sqlite3_column_int(sqlite3_stmt,5))
+                let latitude = Double(sqlite3_column_double(sqlite3_stmt,6))
+                let longitude = Double(sqlite3_column_double(sqlite3_stmt,7))
+                let uploadDate = String(validatingUTF8:sqlite3_column_text(sqlite3_stmt,8))
+                let title = String(validatingUTF8:sqlite3_column_text(sqlite3_stmt,9))
+                
+                
+                // LOAD COMMENTS THIS CODE IS INCORRECT
+                
+                let post = Post(id: postId!,category:categoryName, authorNickname: authorNickname!,
+                                picture: picture!, title:title!, uploadDate:uploadDate!, likes:likes,
+                                dislikes:dislikes, latitude:latitude, longitude:longitude,comments:[])
+                posts.append(post)
+            }
+        }
+        sqlite3_finalize(sqlite3_stmt)
+        return posts
+    }
+    
 }
