d40cc9ba9af7f676d79fc8722858728120b107b5
jezuzcrist <vfxlord@gmail.com>
2017-03-19 06:05:57 -0700

    add comment screen linked with segue still no db connection

---
+++ ranked/addCommentViewController.swift
@@ -0,0 +1,27 @@
+//
+//  addCommentViewController.swift
+//  ranked
+//
+//  Created by macDevMachinVB on 19/03/2017.
+//  Copyright © 2017 Nir Sela. All rights reserved.
+//
+
+import UIKit
+
+class addCommentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancled(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+
+}
